c0c1dbda67e07e35139eac729ea836c866e8937c
Jeremy <jeremytkw98@gmail.com>
2026-02-09 17:10:57 -0800

    ci: add github actions workflow and disable strict react-refresh rule

--- eslint.config.js
+++ eslint.config.js
@@ -13,11 +13,17 @@ export default defineConfig([
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
     ],
+    plugins: {
+      'react-refresh': reactRefresh,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': 'off',
     },
   },
 ])
