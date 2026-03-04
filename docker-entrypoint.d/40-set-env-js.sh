2ec0fde6f5366e4c96d6c93a7008a25c275da7d9
Jeremy Tan <jeremy.tan@ziphq.com>
2026-03-04 19:12:49 +0800

    Keep empty replica metadata when env var unset

--- docker-entrypoint.d/40-set-env-js.sh
+++ docker-entrypoint.d/40-set-env-js.sh
@@ -3,11 +3,6 @@ set -eu
 
 replica_id="${REPLICA_ID:-}"
 
-if [ -z "$replica_id" ]; then
-  sed -i 's/ data-replica-id="__REPLICA_ID__"//g' /usr/share/nginx/html/index.html
-  exit 0
-fi
-
 html_escaped_replica_id=$(printf '%s' "$replica_id" | sed 's/&/\&amp;/g; s/"/\&quot;/g; s/</\&lt;/g; s/>/\&gt;/g')
 escaped_replica_id=$(printf '%s' "$html_escaped_replica_id" | sed 's/[\/&]/\\&/g')
 
